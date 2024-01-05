4ea8b1fec8d1fd505341fc9c03bd7a01c37d50d2
Bryce Dooley <brycehdooley@gmail.com>
2024-01-05 10:06:23 -0500

    aider: Created a table of key/value pairs from the data in the legend session storage.
    # Aider chat conversation:
    
    USER: using the data in the legend session storage, create a table of the key/value pairs
    
    ASSISTANT: To make this change, we need to modify `index.html` and `form.js`:
    
    1. In `index.html`, add a table element with an id of "legend-table" where we will display the key/value pairs.
    2. In `form.js`, after a key/value pair is added to session storage, retrieve the "legend" item, parse it into an object, and use it to create rows in the table.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    index.html
    ```html
    <<<<<<< SEARCH
        <form id="key-value-form">
            <input type="number" id="key-input" placeholder="Enter a number" required />
            <input type="text" id="value-input" placeholder="Enter a letter" required />
            <button type="submit">Add Pair</button>
        </form>
    =======
        <form id="key-value-form">
            <input type="number" id="key-input" placeholder="Enter a number" required />
            <input type="text" id="value-input" placeholder="Enter a letter" required />
            <button type="submit">Add Pair</button>
        </form>
        <table id="legend-table">
            <thead>
                <tr>
                    <th>Number</th>
                    <th>Letter</th>
                </tr>
            </thead>
            <tbody>
            </tbody>
        </table>
    >>>>>>> REPLACE
    ```
    
    form.js
    ```javascript
    <<<<<<< SEARCH
        var legend = JSON.parse(sessionStorage.getItem('legend') || '{}');
        legend[key] = value;
        sessionStorage.setItem('legend', JSON.stringify(legend));
    
        document.getElementById('key-input').value = '';
        document.getElementById('value-input').value = '';
    =======
        var legend = JSON.parse(sessionStorage.getItem('legend') || '{}');
        legend[key] = value;
        sessionStorage.setItem('legend', JSON.stringify(legend));
    
        var table = document.getElementById('legend-table').getElementsByTagName('tbody')[0];
        var row = table.insertRow();
        var cell1 = row.insertCell(0);
        var cell2 = row.insertCell(1);
        cell1.innerHTML = key;
        cell2.innerHTML = value;
    
        document.getElementById('key-input').value = '';
        document.getElementById('value-input').value = '';
    >>>>>>> REPLACE
    ```
    
    This will create a table of the key/value pairs stored in the "legend" item in session storage.

--- form.js
+++ form.js
@@ -8,6 +8,13 @@ document.getElementById('key-value-form').addEventListener('submit', function(ev
     legend[key] = value;
     sessionStorage.setItem('legend', JSON.stringify(legend));
 
+    var table = document.getElementById('legend-table').getElementsByTagName('tbody')[0];
+    var row = table.insertRow();
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = key;
+    cell2.innerHTML = value;
+
     document.getElementById('key-input').value = '';
     document.getElementById('value-input').value = '';
 });
